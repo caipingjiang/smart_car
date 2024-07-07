7a8d3e845b6c4fa9e4e8e2185137485c4b5a9fed
zhang <129734175+3156840672@users.noreply.github.com>
2024-07-07 20:44:08 +0800

    updata.

--- project/mdk/Objects/main.d
+++ project/mdk/Objects/main.d
@@ -1,28 +1,28 @@
 ./objects/main.o: ..\user\src\main.c \
   ..\..\libraries\zf_common\zf_common_headfile.h \
-  D:\keil_5-38-a\ARM\ARMCLANG\Bin\..\include\stdio.h \
-  D:\keil_5-38-a\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  D:\keil_5-38-a\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\keil5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\keil5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\keil5\ARM\ARMCLANG\Bin\..\include\string.h \
   ..\..\libraries\sdk\drives\fsl_common.h \
-  D:\keil_5-38-a\ARM\ARMCLANG\Bin\..\include\assert.h \
-  D:\keil_5-38-a\ARM\ARMCLANG\Bin\..\include\stdbool.h \
-  D:\keil_5-38-a\ARM\ARMCLANG\Bin\..\include\stdlib.h \
-  D:\keil_5-38-a\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\keil5\ARM\ARMCLANG\Bin\..\include\assert.h \
+  C:\keil5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  C:\keil5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  C:\keil5\ARM\ARMCLANG\Bin\..\include\stddef.h \
   ..\..\libraries\sdk\deceive\fsl_device_registers.h \
   ..\..\libraries\sdk\deceive\MIMXRT1064.h \
   ..\..\libraries\sdk\CMSIS\Include\core_cm7.h \
   ..\..\libraries\sdk\CMSIS\Include\cmsis_version.h \
   ..\..\libraries\sdk\CMSIS\Include\cmsis_compiler.h \
   ..\..\libraries\sdk\CMSIS\Include\cmsis_armclang.h \
-  D:\keil_5-38-a\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
-  D:\keil_5-38-a\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\keil5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\keil5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
   ..\..\libraries\sdk\CMSIS\Include\mpu_armv7.h \
   ..\..\libraries\sdk\deceive\system_MIMXRT1064.h \
   ..\..\libraries\sdk\deceive\MIMXRT1064_features.h \
   ..\..\libraries\sdk\drives\fsl_common_arm.h \
   ..\..\libraries\sdk\drives\fsl_clock.h \
   ..\..\libraries\sdk\utilities\debug_console\fsl_debug_console.h \
-  D:\keil_5-38-a\ARM\ARMCLANG\Bin\..\include\stdarg.h \
+  C:\keil5\ARM\ARMCLANG\Bin\..\include\stdarg.h \
   ..\..\libraries\sdk\drives\fsl_iomuxc.h \
   ..\..\libraries\sdk\drives\fsl_cache.h \
   ..\..\libraries\sdk\drives\fsl_csi.h \
@@ -105,5 +105,5 @@
   ..\..\libraries\sdk\board\clock_config.h ..\code\my_moter.h \
   ..\code\my_encoder.h ..\code\my_image.h ..\code\vofa.h \
   ..\code\my_uart.h ..\code\my_servo.h ..\code\filter.h ..\code\my_key.h \
-  ..\code\image.h D:\keil_5-38-a\ARM\ARMCLANG\Bin\..\include\math.h \
+  ..\code\image.h C:\keil5\ARM\ARMCLANG\Bin\..\include\math.h \
   ..\code\imu660ra.h ..\code\car_control.h
